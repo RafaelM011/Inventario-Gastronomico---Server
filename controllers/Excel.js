cc8ac04683f3355a547dd99b389d1e314a795491
ARFS20164207 <ARFS957@gmail.com>
2023-01-07 14:30:35 -0400

    Changes
    importacion de excel

--- controllers/Excel.js
+++ controllers/Excel.js
@@ -1,29 +1,36 @@
 import XLSX from 'xlsx';
+import fs from 'fs';
+
+
 
 export const ReadFile = (req,res) =>  
 {
     const file = XLSX.readFile('data.xlsx');
-    let data = []
+    let data = [];
 
     const sheets = file.SheetNames;
     for(let i = 0; i < sheets.length; i++)
     {
-       let temp = XLSX.utils.sheet_to_json(file.Sheets[sheets[i]])
+       let temp = XLSX.utils.sheet_to_json(file.Sheets[sheets[i]]);
        temp.forEach( sheet => {
          data.push(sheet);
-       })
+       });
     }
 
     res.json(data);
-}
+};
 
 export const WriteFile = (database) => async (req,res) => {
  
     const ingredientsData = await database('ingredientes').select('*').then(data => data);
-    const recipesData = await database('recetas').select('*').then(data => data);
+    const recipesData = await database('recetas')./*where({nombre:"La equina de la doña"}).*/select('*').then(data => data);
+    
+    const user = 'user_1';
+    const filePath = 'TempFiles/'+ user +'.xlsx'; 
+
     const ingredientsWorkSheet = XLSX.utils.json_to_sheet(ingredientsData);
-    const recipesWorkSheet = XLSX.utils.json_to_sheet(recipesData)
-    const book = XLSX.utils.book_new()
+    const recipesWorkSheet = XLSX.utils.json_to_sheet(recipesData);
+    const book = XLSX.utils.book_new();
     const opts = { bookType:"xlsx", bookSST:false, type:"array"};      
     book.Props = {
         Title: "Inventario Gastronómico",
@@ -32,10 +39,29 @@ export const WriteFile = (database) => async (req,res) => {
         CreatedDate: new Date()
     };
     
-    XLSX.utils.book_append_sheet(book,ingredientsWorkSheet,"Ingredientes") 
-    XLSX.utils.book_append_sheet(book,recipesWorkSheet,"Recetas") 
+    XLSX.utils.book_append_sheet(book,ingredientsWorkSheet,"Ingredientes") ;
+    XLSX.utils.book_append_sheet(book,recipesWorkSheet,"Recetas") ;
     
-    XLSX.writeFileXLSX(book,'Inventario_Gastronomico.xlsx',opts)
-    // console.log(book.Sheets.Recetas.B2);
-    res.json('El archivo de excel ha sido creado.');
-}
+    XLSX.writeFileXLSX(book,filePath,opts);
+    res.download(filePath);
+};
+
+export const ImportingXLSX = (database) => async (req,res)=>  
+{
+    console.log(req.file.path);
+    const file = XLSX.readFile(req.file.path);
+    console.log(file);
+    
+    let data = [];
+    const sheets = file.SheetNames;
+    for(let i = 0; i < sheets.length; i++)
+    {
+       let temp = XLSX.utils.sheet_to_json(file.Sheets[sheets[i]]);
+       temp.forEach( sheet => {
+         data.push(sheet);
+       });
+    }
+    console.log(data);
+
+    res.json(data);
+};
--- server.js
+++ server.js
@@ -9,11 +9,14 @@ import SignIn from './controllers/SignIn.js';
 import { AddSucursal, ImportSucursales } from './controllers/ManageSucursales.js';
 import { ImportIngredientes, AddIngredient, DecreaseIngredientAmount, RemoveIngredient, UpgradeIngredientInfo } from './controllers/ManageIngredient.js';
 import { AddRecipe, ImportRecipes, UpdateRecipe } from './controllers/ManageRecipes.js';
-import { ReadFile, WriteFile } from './controllers/Excel.js';
+import { ImportingXLSX, ReadFile, WriteFile } from './controllers/Excel.js';
+
+import multer from 'multer';
 
 //Server
 const server = express();
-const PORT = process.env.PORT ?? 4000;
+const PORT = process.env.PORT ?? 4050;
+const upload = multer({dest: 'uploads/'});
 server.listen(PORT, () =>{
     console.log(`App is running on port ${PORT}`);
 });
@@ -27,60 +30,61 @@ const database = knex({
     client: 'pg',
     connection: {
       host : process.env.PGHOST ?? '127.0.0.1',
-      port : process.env.PGPORT ?? 5432,
+      port : process.env.PGPORT ?? 4000,
       user : process.env.PGUSER ?? 'postgres',
       password : process.env.PGPASSWORD ?? process.env.POSTGRESQL_DATABASE_PASSWORD,
       database : process.env.PGDATABASE ?? 'Inventario'
     }
-})
+});
 
 // Server startup
-server.get('/', (req,res) => res.json("App is running properly"))
+server.get('/', (req,res) => res.json("App is running properly"));
 
 /* CREDENTIALS */
 
 //Put data to register a new user
-server.put('/register', Register(database, bcrypt))
+server.put('/register', Register(database, bcrypt));
 //Post data to log in
-server.post('/signin', SignIn(database, bcrypt))
+server.post('/signin', SignIn(database, bcrypt));
 
 /* INGREDIENTS*/ 
 
 //Post data from database/excel
-server.post('/importingredientes', ImportIngredientes(database))
+server.post('/importingredientes', ImportIngredientes(database));
 //Put ingredients into database
-server.put('/agregaringrediente', AddIngredient(database))
+server.put('/agregaringrediente', AddIngredient(database));
 //Delete ingredients from database
-server.delete('/removeingredient', RemoveIngredient(database))
+server.delete('/removeingredient', RemoveIngredient(database));
 //Decrease ingredientes from recipe info on database
-server.post('/decreaseingredient', DecreaseIngredientAmount(database))
+server.post('/decreaseingredient', DecreaseIngredientAmount(database));
 // Update ingredients info on database
-server.post('/updateingredients', UpgradeIngredientInfo(database))
+server.post('/updateingredients', UpgradeIngredientInfo(database));
 
 /* SUCURSALES */
 
 //Post data from database/excel
-server.post('/importsucursales', ImportSucursales(database))
+server.post('/importsucursales', ImportSucursales(database));
 //Post new sucursal to datbase
-server.post('/addsucursal', AddSucursal(database))
+server.post('/addsucursal', AddSucursal(database));
 
 
 /* RECIPES */
 
 //Add recipe to database
-server.put('/addrecipe', AddRecipe(database))
+server.put('/addrecipe', AddRecipe(database));
 //Get recipes from database
-server.post('/getrecipes', ImportRecipes(database))
+server.post('/getrecipes', ImportRecipes(database));
 //Update recipes on database
-server.post('/updaterecipe', UpdateRecipe(database))
+server.post('/updaterecipe', UpdateRecipe(database));
 
 /* EXCEL */
 
 //Read excel uploaded from client
-server.get('/readexcel', ReadFile)
+server.get('/readexcel', ReadFile);
 //Write excel from database info
-server.get('/writeexcel', WriteFile(database))
-
+server.get('/writeexcel', WriteFile(database));
+server.post('/ImportXLSX', upload.single('file'),WriteFile(database));
+ 
 
 // server.get('/download', (req,res) => {
 //     res.download('./elliott-wave-principle.pdf')
